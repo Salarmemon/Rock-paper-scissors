4355a85dc4abab5e31f096777c06bb4ea0b77310
Salar Memon <142799802+Salarmemon@users.noreply.github.com>
2026-04-03 18:26:33 +0500

    Create utility functions for comparing choices and decide winner

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+//utility functions
+
+// tartget 1elements in a convenient way\
+const getElm = (value, method, isAll=false) => {
+    return method === "id" ? 
+    document.getElementById(value) :
+     method === "class" && !isAll ?
+     document.getElementsByClassName(value) :
+     method === "class" && isAll ?
+     document.querySelectorAll(`.${value}`) :
+     "Method not supported";
+}
+
+
+// compare choices 
+const decideRound = (player, computer) => {
+    if (player === computer) {
+        return `it's a tie: ${player} vs ${computer}`;
+    } else if ((player === "rock" && computer === "scissors") || 
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")) {
+
+        playerScore++;
+        return `You win: ${player} beats ${computer}`;
+        
+
+    } else {
+        computerScore++;
+        return `You lost: ${computer} beats ${player}`;
+        
+    }
+}
+
+// variables and element references
+const choiceBtns = getElm("choice-btn", "class", true);
+const playerScoreElm = getElm("player-score", "id");
+const computerScoreElm = getElm("computer-score");
+const resultDialog = getElm("result-dialog", "class");
+const numOfRounds = 5;
+let playerScore = 0;
+let computerScore = 0;
+// generate computer's choice
+
+const generateComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor((Math.random() * 3));
+    return choices[randomIndex];
+}
+
+
+// hover sound effects
 let lastPlayed = 0;
 
 const playSoundHover = () => {
@@ -9,9 +60,15 @@ const playSoundHover = () => {
     sound.play();
     lastPlayed = now;
 }
-
-const choiceBtns = document.querySelectorAll(".choice-btn");
-
+choiceBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const computerChoice = generateComputerChoice();
+    const playerChoice = e.target.id;
+    
+    const roundResult = decideRound(playerChoice, computerChoice);
+    console.log(roundResult);
+})
+})
 choiceBtns.forEach((btn) => {
     btn.addEventListener("mouseenter", playSoundHover);
 })
